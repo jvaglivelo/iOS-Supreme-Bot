412fe2c688d662684c82b492c2f37e62ef4031a5
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2020-02-28 13:39:25 -0500

    Initial Commit

---
+++ iOS-Supreme-Bot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Supreme-Bot
+//
+//  Created by Jordan Vaglivelo on 2/28/20.
+//  Copyright © 2020 Jordan Vaglivelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
